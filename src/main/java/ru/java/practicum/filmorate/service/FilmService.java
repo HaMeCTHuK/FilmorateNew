13fce2275876d7de85d1fa9b3395e4f19b3425ac
EgorovArtur <arthurmsk12154@yandex.ru>
2024-02-02 18:04:19 +0300

    Добавил методы для поиска фильма (add-search)

--- src/main/java/ru/java/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/java/practicum/filmorate/service/FilmService.java
@@ -1,6 +1,5 @@
 package ru.java.practicum.filmorate.service;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
